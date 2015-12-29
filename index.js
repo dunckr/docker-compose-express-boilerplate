872ab98b02a3bdf438c7f4b2d590c93799bc7d1c
Duncan Beaton <dunckr@gmail.com>
2015-12-29 11:36:27 +0000

    fix cd into app dir

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.set('message', 'Hello World!');
+app.set('port', 3000)
+app.set('message', 'Hello World!')
+
 app.get('/', (req, res) => {
-  res.send(app.get('message'));
-});
+  res.send(app.get('message'))
+})
 
-app.listen(process.env.PORT || '3000');
+app.listen(process.env.PORT || app.get('port'))
 
-module.exports = app;
+module.exports = app
--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
-var test = require('tape');
-var request = require('supertest');
-var app = require('../index');
+var test = require('tape')
+var request = require('supertest')
+var app = require('../index')
 
 test('server', (t) => {
   request(app)
   .get('/')
   .end((err, res) => {
-    t.equals(err, null);
-    t.equals(res.status, 200);
-    t.equals(res.text, app.get('message'));
-    t.end();
-  });
-});
+    t.equals(err, null)
+    t.equals(res.status, 200)
+    t.equals(res.text, app.get('message'))
+    t.end()
+  })
+})
